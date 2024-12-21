7ac95271c3aaab83c0a8cc69479a06363fd47b53
gitMFQ <652299482@qq.com>
2024-12-21 17:36:44 +0800

    page

---
+++ page.js
@@ -0,0 +1,20 @@
+const headerEl = document.querySelector("header");
+const scrollToTop = document.querySelector(".scrollToTop");
+//  监听窗口滑动事件
+window.addEventListener("scroll", () => {
+  let height = headerEl.getBoundingClientRect().height;    // 获取所占屏幕空间的矩形区域高度
+  // 向下滑动距离超过800
+  if (window.pageYOffset - height > 800) {
+    if (!headerEl.classList.contains("sticky")) {    // 先判断是否有这个属性，有就不添加，没有就添加
+      headerEl.classList.add("sticky");
+    }
+  } else {
+    headerEl.classList.remove("sticky");
+  }
+  // 向下滑动超过2000像素 就显示
+  if (window.pageYOffset > 800) {
+    scrollToTop.style.display = "block";
+  } else {
+    scrollToTop.style.display = "none";
+  }
+})
